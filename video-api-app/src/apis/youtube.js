7c8f91400dcdbc1d8bd970d167c6564235451d1f
Alex Cho <alexsjcho@gmail.com>
2019-03-22 16:44:41 -0700

    implemented axios, created youtube.js file with youtube api key, etc

--- video-api-app/src/apis/youtube.js
+++ video-api-app/src/apis/youtube.js
@@ -1,4 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDHT4KvQZ68PAeT5bnTshygqegVUuuhon4";
 
-//YouTube API Key
-//
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
