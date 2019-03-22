65190299ccdb60e5b4d75b9a308ae9020c03707a
Alex Cho <alexsjcho@gmail.com>
2019-03-22 16:09:36 -0700

    first commit

---
+++ video-api-app/src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return <div className="ui segment">Hello</div>;
+  }
+}
+
+export default SearchBar;
---
+++ video-api-app/src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDom from " react-dom";
+import App from "./components/App";
+import SearchBar from "./components/SearchBar";
+
+ReactDom.render(<App />, document.querySelector("#root"));
