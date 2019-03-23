9be6c3d5d7f853bfc15357a163b598e32f13ec0a
Alex Cho <alexsjcho@gmail.com>
2019-03-23 15:57:35 -0700

    passing props from child to parent

--- video-api-app/src/components/App.js
+++ video-api-app/src/components/App.js
@@ -4,7 +4,7 @@ import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 
 class App extends Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -14,12 +14,20 @@ class App extends Component {
     });
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = video => {
+    console.log("from the App component!", video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <h1>App Component</h1>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
