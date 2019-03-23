08256f826877d839efe4ee9f495b1c063639472e
Alex Cho <alexsjcho@gmail.com>
2019-03-23 15:08:47 -0700

    made request to youtube aip

--- video-api-app/src/components/App.js
+++ video-api-app/src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends Component {
+  onTermSubmit = term => {
+    youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+  };
   render() {
     return (
       <div className="ui container">
         <h1>App Component</h1>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
